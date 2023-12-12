71715b86b70b30e6992a5a3a9d566344e791987a
326eunjin <326eunjin@naver.com>
2023-12-12 19:10:42 +0900

    실시간 위치 반영 성공

--- src/home/home.jsx
+++ src/home/home.jsx
@@ -1,115 +1,145 @@
-import React, { useEffect, useState, useCallback } from "react";
+import React, { useEffect, useRef, useState, useCallback } from "react";
+import axios from "axios";
 
-const MapComponent = () => {
-  const [map, setMap] = useState(null);
+const useGeolocation = () => {
+  const watchId = useRef(null);
 
-  // useCallback을 사용하여 의존성 배열이 변하지 않도록 함
-  const handleNoGeolocation = useCallback((map) => {
-    const locPosition = new window.kakao.maps.LatLng(33.450701, 126.570667);
-    const message = "geolocation 사용이 허용되지 않았습니다.";
-    displayMarker(map, locPosition, message);
+  const [geolocationInfo, setGeolocationInfo] = useState({
+    isLoaded: false,
+    error: null,
+    latitude: null,
+    longitude: null,
+  });
+
+  const clearWatch = useCallback(() => {
+    if (watchId.current) {
+      navigator.geolocation.clearWatch(watchId.current);
+      watchId.current = null;
+    }
+  }, [watchId]);
+
+  useEffect(() => {
+    const options = {
+      enableHighAccuracy: false,
+      maximumAge: Infinity,
+      timeout: 100000,
+    };
+
+    const onEventSuccess = ({ coords }) => {
+      const { latitude, longitude } = coords;
+      setGeolocationInfo((prev) => ({
+        ...prev,
+        isLoaded: true,
+        latitude,
+        longitude,
+      }));
+    };
+
+    const onEventError = (error) => {
+      setGeolocationInfo((prev) => ({
+        ...prev,
+        error,
+      }));
+    };
+
+    if (!navigator.geolocation) return;
+
+    watchId.current = navigator.geolocation.watchPosition(
+      onEventSuccess,
+      onEventError,
+      options
+    );
+
+    return () => {
+      clearWatch();
+    };
+  }, [clearWatch]);
+
+  return geolocationInfo;
+};
+
+const LocationTracker = () => {
+  const geolocationInfo = useGeolocation();
+  const mapContainerRef = useRef(null);
+  const kakaoMap = useRef(null);
+
+  useEffect(() => {
+    if (!kakaoMap.current) {
+      // Initialize the map only if it doesn't exist
+      const mapOption = {
+        center: new window.kakao.maps.LatLng(33.450701, 126.570667),
+        level: 2,
+      };
+      kakaoMap.current = new window.kakao.maps.Map(
+        mapContainerRef.current,
+        mapOption
+      );
+    }
   }, []);
 
   useEffect(() => {
-    const mapContainer = document.getElementById("map");
-    const initialCenter = new window.kakao.maps.LatLng(33.450701, 126.570667);
+    if (geolocationInfo.isLoaded && kakaoMap.current) {
+      const { latitude, longitude } = geolocationInfo;
 
-    const options = {
-      center: initialCenter,
-      level: 10,
-    };
+      // Your API endpoint in Django
+      const apiUrl = `/restaurants/nearby?latitude=${latitude}&longitude=${longitude}`;
 
-    const newMap = new window.kakao.maps.Map(mapContainer, options);
-    setMap(newMap);
+      // Make a POST request to your Django backend
+      axios
+        .get(apiUrl)
+        .then((response) => {
+          console.log("Location data sent successfully:", response.data);
+        })
+        .catch((error) => {
+          console.error("Error sending location data:", error);
+        });
 
-    if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(
-        (position) => {
-          const { latitude, longitude } = position.coords;
-          const locPosition = new window.kakao.maps.LatLng(latitude, longitude);
-          const message = '<div style="padding:5px;">여기에 계신가요?!</div>';
-          displayMarker(newMap, locPosition, message);
-        },
-        (error) => {
-          console.error(error.message);
-          // useCallback으로 감싸진 함수를 호출
-          handleNoGeolocation(newMap);
-        }
+      var imageSrc =
+          "https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/marker_red.png", // 마커이미지의 주소입니다
+        imageSize = new window.kakao.maps.Size(64, 69), // 마커이미지의 크기입니다
+        imageOption = { offset: new window.kakao.maps.Point(27, 69) }; // 마커이미지의 옵션입니다. 마커의 좌표와 일치시킬 이미지 안에서의 좌표를 설정합니다.
+
+      // 마커의 이미지정보를 가지고 있는 마커이미지를 생성합니다
+      var markerImage = new window.kakao.maps.MarkerImage(
+        imageSrc,
+        imageSize,
+        imageOption
       );
+      const locPosition = new window.kakao.maps.LatLng(latitude, longitude); // 마커가 표시될 위치입니다
+      if (!kakaoMap.current.marker) {
+        const marker = new window.kakao.maps.Marker({
+          position: locPosition,
+          image: markerImage,
+          map: kakaoMap.current,
+        });
+        kakaoMap.current.marker = marker;
+      } else {
+        kakaoMap.current.marker.setPosition(locPosition);
+      }
 
-      // Watch position to update map in real-time
-      const watchId = navigator.geolocation.watchPosition(
-        (position) => {
-          const { latitude, longitude } = position.coords;
-          const locPosition = new window.kakao.maps.LatLng(latitude, longitude);
-          moveMarker(newMap, locPosition);
-        },
-        (error) => {
-          console.error(error.message);
-        }
-      );
-
-      // Cleanup watchPosition on component unmount
-      return () => navigator.geolocation.clearWatch(watchId);
-    } else {
-      // useCallback으로 감싸진 함수를 호출
-      handleNoGeolocation(newMap);
+      kakaoMap.current.setCenter(locPosition);
     }
-  }, [handleNoGeolocation]); // handleNoGeolocation 함수를 의존성 배열에 추가
-
-  const displayMarker = (map, locPosition, message) => {
-    const marker = new window.kakao.maps.Marker({
-      map: map,
-      position: locPosition,
-    });
-
-    const infowindow = new window.kakao.maps.InfoWindow({
-      content: message,
-      removable: true,
-    });
-
-    infowindow.open(map, marker);
-    map.setCenter(locPosition);
-  };
-
-  const moveMarker = (map, locPosition) => {
-    if (map) {
-      map.panTo(locPosition);
-    }
-  };
-
-  const handleCenterMap = () => {
-    if (map) {
-      navigator.geolocation.getCurrentPosition(
-        (position) => {
-          const { latitude, longitude } = position.coords;
-          const locPosition = new window.kakao.maps.LatLng(latitude, longitude);
-          map.panTo(locPosition);
-        },
-        (error) => {
-          console.error(error.message);
-        }
-      );
-    }
-  };
-
-  const buttonStyle = {
-    position: "absolute",
-    top: "10px",
-    left: "10px",
-    zIndex: "1",
-  };
+  }, [geolocationInfo]);
 
   return (
-    <div style={{ position: "relative" }}>
-      <div id="map" style={{ width: "100%", height: "100vh" }}></div>
-      {map && (
-        <button style={buttonStyle} onClick={handleCenterMap}>
-          Center Map
-        </button>
+    <div>
+      <h2>실시간 위치 추적기</h2>
+      <div ref={mapContainerRef} style={{ width: "100%", height: "100vh" }} />
+      {geolocationInfo.isLoaded ? (
+        <div>
+          <p>위도: {geolocationInfo.latitude}</p>
+          <p>경도: {geolocationInfo.longitude}</p>
+        </div>
+      ) : (
+        <p>위치 로딩 중...</p>
+      )}
+      {geolocationInfo.error && (
+        <p>
+          위치 정보를 가져오는 동안 오류 발생: {geolocationInfo.error.message}
+        </p>
       )}
     </div>
   );
 };
 
-export default MapComponent;
+export default LocationTracker;
--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,10 +1,59 @@
-const { createProxyMiddleware } = require('http-proxy-middleware');
+const { createProxyMiddleware } = require("http-proxy-middleware");
 
-module.exports = function(app){
+module.exports = function (app) {
+  // Proxy for Django admin
   app.use(
-    createProxyMiddleware('/yumyum', {
-      target: 'https://yumyum-backend-48405822bc43.herokuapp.com',
+    "/admin",
+    createProxyMiddleware({
+      target: "http://localhost:8000",
       changeOrigin: true,
     })
-  )
-};  
+  );
+
+  // Proxy for Django users
+  app.use(
+    "/users",
+    createProxyMiddleware({
+      target: "http://localhost:8000",
+      changeOrigin: true,
+    })
+  );
+
+  // Proxy for Django reviews
+  app.use(
+    "/reviews",
+    createProxyMiddleware({
+      target: "http://localhost:8000",
+      changeOrigin: true,
+    })
+  );
+
+  // Proxy for Django restaurants
+  app.use(
+    "/restaurants",
+    createProxyMiddleware({
+      target: "http://localhost:8000",
+      changeOrigin: true,
+    })
+  );
+
+  // Add more proxy rules for other Django endpoints as needed
+
+  // Proxy for Django Swagger
+  app.use(
+    /^\/swagger(\/.*)?$/,
+    createProxyMiddleware({
+      target: "http://localhost:8000",
+      changeOrigin: true,
+    })
+  );
+
+  // Proxy for Django Redoc
+  app.use(
+    "/redoc",
+    createProxyMiddleware({
+      target: "http://localhost:8000",
+      changeOrigin: true,
+    })
+  );
+};
