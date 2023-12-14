3629d682442b84150469f48ee6cd43b032af3af5
326eunjin <326eunjin@naver.com>
2023-12-14 15:00:46 +0900

    warning disabled

--- src/restaurantsearch/restaurantsearch.jsx
+++ src/restaurantsearch/restaurantsearch.jsx
@@ -1,56 +1,74 @@
-import React, { useEffect, useState } from 'react';
-import './restaurantsearch.css';
+/* eslint-disable */
+import React, { useEffect, useState } from "react";
+import "./restaurantsearch.css";
 import testrestaurantlogo from "./images/subway.png";
 import testrestaurantmap from "./images/subwaymap.png";
 import { useNavigate } from "react-router-dom";
-import axios from 'axios';
+import axios from "axios";
 
 const RestaurantCard = ({ restaurant }) => {
   const navigate = useNavigate();
 
-  function newreview(){
+  function newreview() {
     const restaurantId = restaurant.restaurant_id;
     const restaurantName = restaurant.name;
-    navigate(`/newreview`, {state: { id : restaurantId, name : restaurantName },
+    navigate(`/newreview`, {
+      state: { id: restaurantId, name: restaurantName },
     });
   }
 
   return (
     <div className="searchcontentinnercontainer">
-        <div className="searchcontentcontainer">
-          <img className="searchrestaurantlogo" src={testrestaurantlogo} alt="Logo" />  
-          <span className="searchnametag">{restaurant.name}</span>
-          <span className="searchcontexttag">#간편 #다이어트 #샌드위치</span>
-          <span className="searchcontexttag">리뷰 평점 : {restaurant.score}점</span>
-          <span className="searchcontexttag">운영시간 : {restaurant.opentime} - {restaurant.closetime}</span>
-          <span className="searchcontexttag">위치 : {restaurant.loacation}</span>
-          <img className="searchrestaurantmap" src={testrestaurantmap} alt="Logo" />
-          <span className="searchparttag">메뉴 구성</span>
-          <div className="restarantmenulist">
-            <span className="restaurantcontexttag">관리자페이지 추가 후 구현 예정</span>
-            <button className='showallmenubutton'>전체 보기</button>
-          </div>
-
-          <span className="searchparttag">공지 사항</span>
-          <div className="restarantmenulist">
-            <span className="restaurantcontexttag">관리자페이지 추가 후 구현 예정</span>
-            <button className='showallmenubutton'>전체 보기</button>
-          </div>
-          ㄴ
-          <span className="searchparttag">이용 후기</span>
-          <div className="restarantmenulist">
-            <span className="restaurantcontexttag">이용후기 예시</span>
-            <button className='showallmenubutton' onClick={newreview}>리뷰 작성</button>
-          </div>
-          <span className="searchparttag">대기 현황</span>
-          <div className="restarantmenulist">
-            <span className="restaurantwaitingtag1">평균 대기 시간 : </span>
-            <span className="restaurantwaitingtag2">현재 대기중인 팀 : </span>
-            <span className="restaurantwaitingtag3">예상 대기 시간 : </span>
-            <button className='showallmenubutton'>줄서기</button>
-          </div>
+      <div className="searchcontentcontainer">
+        <img
+          className="searchrestaurantlogo"
+          src={testrestaurantlogo}
+          alt="Logo"
+        />
+        <span className="searchnametag">{restaurant.name}</span>
+        <span className="searchcontexttag">#간편 #다이어트 #샌드위치</span>
+        <span className="searchcontexttag">
+          리뷰 평점 : {restaurant.score}점
+        </span>
+        <span className="searchcontexttag">
+          운영시간 : {restaurant.opentime} - {restaurant.closetime}
+        </span>
+        <span className="searchcontexttag">위치 : {restaurant.loacation}</span>
+        <img
+          className="searchrestaurantmap"
+          src={testrestaurantmap}
+          alt="Logo"
+        />
+        <span className="searchparttag">메뉴 구성</span>
+        <div className="restarantmenulist">
+          <span className="restaurantcontexttag">
+            관리자페이지 추가 후 구현 예정
+          </span>
+          <button className="showallmenubutton">전체 보기</button>
         </div>
-          <button className="searchbackbutton">뒤로 가기</button>
+        <span className="searchparttag">공지 사항</span>
+        <div className="restarantmenulist">
+          <span className="restaurantcontexttag">
+            관리자페이지 추가 후 구현 예정
+          </span>
+          <button className="showallmenubutton">전체 보기</button>
+        </div>
+        ㄴ<span className="searchparttag">이용 후기</span>
+        <div className="restarantmenulist">
+          <span className="restaurantcontexttag">이용후기 예시</span>
+          <button className="showallmenubutton" onClick={newreview}>
+            리뷰 작성
+          </button>
+        </div>
+        <span className="searchparttag">대기 현황</span>
+        <div className="restarantmenulist">
+          <span className="restaurantwaitingtag1">평균 대기 시간 : </span>
+          <span className="restaurantwaitingtag2">현재 대기중인 팀 : </span>
+          <span className="restaurantwaitingtag3">예상 대기 시간 : </span>
+          <button className="showallmenubutton">줄서기</button>
+        </div>
+      </div>
+      <button className="searchbackbutton">뒤로 가기</button>
     </div>
   );
 };
@@ -59,7 +77,9 @@ const RestaurantSlider = ({ restaurants }) => {
   const [currentPage, setCurrentPage] = useState(0);
 
   const handlePrevPage = () => {
-    setCurrentPage(currentPage - 1 < 0 ? restaurants.length - 1 : currentPage - 1);
+    setCurrentPage(
+      currentPage - 1 < 0 ? restaurants.length - 1 : currentPage - 1
+    );
   };
 
   const handleNextPage = () => {
@@ -70,62 +90,91 @@ const RestaurantSlider = ({ restaurants }) => {
     <div className="searchbackground">
       <div className="searchtotalcontainer">
         <RestaurantCard restaurant={restaurants[currentPage]} />
-        <div className='changepageslide'>
-          <button className="searchslidebutton" onClick={handlePrevPage}>이전 결과</button>
-          <button className="searchslidebutton" onClick={handleNextPage}>다음 결과</button>
-          </div>
+        <div className="changepageslide">
+          <button className="searchslidebutton" onClick={handlePrevPage}>
+            이전 결과
+          </button>
+          <button className="searchslidebutton" onClick={handleNextPage}>
+            다음 결과
+          </button>
+        </div>
       </div>
     </div>
   );
 };
 
 const RestaurantPage = () => {
-  
   const [restaurantlist, setrestaurantlist] = useState([]);
-  
-  useEffect(() => {
 
-    const token = localStorage.getItem('token');  // Replace 'your_token_key' with the actual key used for the token
+  useEffect(() => {
+    const token = localStorage.getItem("token"); // Replace 'your_token_key' with the actual key used for the token
 
     const instance = axios.create({
-        baseURL: "https://yumyum-backend-48405822bc43.herokuapp.com",
-        headers: {
-          "Content-Type": "application/json",
-          "Authorization" : `Bearer ${token}`,
-        },
-      })
+      baseURL: "https://yumyum-backend-48405822bc43.herokuapp.com",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+    });
 
     // Fetch data from the server using axios
-    instance.get('/restaurants/filtered', {
+    instance
+      .get("/restaurants/filtered", {
         params: {
-            latitude: 37.50726305,
-            longitude: 126.9599876,
+          latitude: 37.50726305,
+          longitude: 126.9599876,
         },
-        })
-        .then(response => {
-            console.log(response.data.restaurants);
-            setrestaurantlist(response.data.restaurants);
-            console.log(restaurantlist);
-        })
-        .catch(error => {
-            console.error('Error fetching data:', error);
-        });
-}, []); 
+      })
+      .then((response) => {
+        console.log(response.data.restaurants);
+        setrestaurantlist(response.data.restaurants);
+        console.log(restaurantlist);
+      })
+      .catch((error) => {
+        console.error("Error fetching data:", error);
+      });
+  }, []);
 
-  
   const restaurants = [
-    { restaurant_id : 1 , name: '서브웨이 숭실대점', score: 4.1, opentime:"07:00", closetime:"22:00", loacation:"서울 동작구 사당로 10 지층 101호"},
-    { restaurant_id : 2 , name: '샐러데이즈 상도점', score: 4.58, opentime:"09:00", closetime:"20:00", loacation:"서울 동작구 상도로 337-1 1층"},
-    { restaurant_id : 3 , name: '우마이', score: 4.53, opentime:"11:00", closetime:"19:00", loacation:"서울 동작구 상도로 67길 27 3층"},
-    { restaurant_id : 3 , name: '우마이', score: 4.53, opentime:"11:00", closetime:"19:00", loacation:"서울 동작구 상도로 67길 27 3층"},
+    {
+      restaurant_id: 1,
+      name: "서브웨이 숭실대점",
+      score: 4.1,
+      opentime: "07:00",
+      closetime: "22:00",
+      loacation: "서울 동작구 사당로 10 지층 101호",
+    },
+    {
+      restaurant_id: 2,
+      name: "샐러데이즈 상도점",
+      score: 4.58,
+      opentime: "09:00",
+      closetime: "20:00",
+      loacation: "서울 동작구 상도로 337-1 1층",
+    },
+    {
+      restaurant_id: 3,
+      name: "우마이",
+      score: 4.53,
+      opentime: "11:00",
+      closetime: "19:00",
+      loacation: "서울 동작구 상도로 67길 27 3층",
+    },
+    {
+      restaurant_id: 3,
+      name: "우마이",
+      score: 4.53,
+      opentime: "11:00",
+      closetime: "19:00",
+      loacation: "서울 동작구 상도로 67길 27 3층",
+    },
     // 추가적인 레스토랑 데이터
   ];
 
   return (
-    <div >
-      <RestaurantSlider restaurants={restaurants} /> 
+    <div>
+      <RestaurantSlider restaurants={restaurants} />
     </div>
-  
   );
 };
 
--- src/thread/thread.jsx
+++ src/thread/thread.jsx
@@ -1,116 +1,133 @@
-import React, { useEffect, useState } from 'react';
-import './thread.css';
-import EmptyHeart from './images/emptyheart.png';
-import FilledHeart from './images/emptyheart.png';
-import axios from 'axios';
-import { useNavigate } from 'react-router-dom';
+/* eslint-disable */
+import React, { useEffect, useState } from "react";
+import "./thread.css";
+import EmptyHeart from "./images/emptyheart.png";
+import FilledHeart from "./images/emptyheart.png";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 const ThreadItem = ({ thread, onCancelReservation, onMenuBrowse }) => {
-    const [isLiked, setIsLiked] = useState(false);
+  const [isLiked, setIsLiked] = useState(false);
 
-    const handleHeartClick = () => {
-        setIsLiked(!isLiked);
-        console.log("heart")
-        // 여기에서 다른 처리를 추가할 수 있습니다.
-    };
+  const handleHeartClick = () => {
+    setIsLiked(!isLiked);
+    console.log("heart");
+    // 여기에서 다른 처리를 추가할 수 있습니다.
+  };
 
-    let keywordbox = ['#한식', '#중식', '#일식', '#샐러드'];
+  let keywordbox = ["#한식", "#중식", "#일식", "#샐러드"];
 
-    return (
-        <div className="threadtotalcontainer">
-            <div className="threadUserHeader">
-                <div className="threadUserIcon"></div>
-                <span className="threadusername">{thread.user_name} </span>
-            </div>
-            <div className="threadblockcontainer">
-                <div className="threadblocknameandheart">
-                    <span className="threadblockrestaurantname">{thread.restaurant_name}</span>
-                    <img
-                        className={`likeHeart ${isLiked ? 'liked' : ''}`}
-                        src={isLiked ? FilledHeart : EmptyHeart}
-                        alt={isLiked ? 'likedheartLogo' : 'emptyheartLogo'}
-                        onClick={handleHeartClick}
-                    />
-                </div>
-                <span className="threadblockrestaurantkeyword">{keywordbox[(thread.category / 100) - 1]}</span>
-                <span className="threadblockrestaurantstars">별점 : {thread.stars}</span>
-                <span className="threadblockrestaurantcomment">{thread.contents}</span>
-            </div>
+  return (
+    <div className="threadtotalcontainer">
+      <div className="threadUserHeader">
+        <div className="threadUserIcon"></div>
+        <span className="threadusername">{thread.user_name} </span>
+      </div>
+      <div className="threadblockcontainer">
+        <div className="threadblocknameandheart">
+          <span className="threadblockrestaurantname">
+            {thread.restaurant_name}
+          </span>
+          <img
+            className={`likeHeart ${isLiked ? "liked" : ""}`}
+            src={isLiked ? FilledHeart : EmptyHeart}
+            alt={isLiked ? "likedheartLogo" : "emptyheartLogo"}
+            onClick={handleHeartClick}
+          />
         </div>
-    );
+        <span className="threadblockrestaurantkeyword">
+          {keywordbox[thread.category / 100 - 1]}
+        </span>
+        <span className="threadblockrestaurantstars">
+          별점 : {thread.stars}
+        </span>
+        <span className="threadblockrestaurantcomment">{thread.contents}</span>
+      </div>
+    </div>
+  );
 };
 
 const ThreadList = ({ threads, onCancelReservation, onMenuBrowse }) => {
-    return (
-        <div className="review-list">
-            {threads.map((thread, index) => (
-                <ThreadItem key={index} thread={thread} onCancelReservation={onCancelReservation} onMenuBrowse={onMenuBrowse} />
-            ))}
-        </div>
-    );
+  return (
+    <div className="review-list">
+      {threads.map((thread, index) => (
+        <ThreadItem
+          key={index}
+          thread={thread}
+          onCancelReservation={onCancelReservation}
+          onMenuBrowse={onMenuBrowse}
+        />
+      ))}
+    </div>
+  );
 };
 
 const Thread = () => {
+  const navigate = useNavigate();
 
-    const navigate = useNavigate();
+  const [reviews, setReviews] = useState([]);
 
-    const [reviews, setReviews] = useState([]);
+  useEffect(() => {
+    const token = localStorage.getItem("token"); // Replace 'your_token_key' with the actual key used for the token
 
-    useEffect(() => {
+    const instance = axios.create({
+      baseURL: "https://yumyum-backend-48405822bc43.herokuapp.com",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+    });
 
-        const token = localStorage.getItem('token');  // Replace 'your_token_key' with the actual key used for the token
+    // Fetch data from the server using axios
+    instance
+      .get("/reviews/thread", {
+        params: {
+          latitude: 37.50726305,
+          longitude: 126.9599876,
+        },
+      })
+      .then((response) => {
+        console.log(response.data.reviews);
+        setReviews(response.data.reviews);
+        console.log(reviews);
+        // Handle the response and update the state
+      })
+      .catch((error) => {
+        console.error("Error fetching data:", error);
+      });
+  }, []);
 
-        const instance = axios.create({
-            baseURL: "https://yumyum-backend-48405822bc43.herokuapp.com",
-            headers: {
-              "Content-Type": "application/json",
-              "Authorization" : `Bearer ${token}`,
-            },
-          })
+  const handleCancelReservation = (storeName) => {
+    console.log(`예약이 취소된 가게: ${storeName}`);
+    // 여기에서 예약 취소 로직을 추가할 수도 있습니다.
+  };
 
-        // Fetch data from the server using axios
-        instance.get('/reviews/thread', {
-            params: {
-                latitude: 37.50726305,
-                longitude: 126.9599876,
-            },
-            })
-            .then(response => {
-                console.log(response.data.reviews);
-                setReviews(response.data.reviews);
-                console.log(reviews);
-                // Handle the response and update the state
-                
-            })
-            .catch(error => {
-                console.error('Error fetching data:', error);
-            });
-    }, []); 
+  const handleMenuBrowse = (rating) => {
+    console.log(`현재 대기 인원: ${rating}`);
+    // 여기에서 메뉴 둘러보기 로직을 추가할 수도 있습니다.
+  };
 
-    const handleCancelReservation = (storeName) => {
-        console.log(`예약이 취소된 가게: ${storeName}`);
-        // 여기에서 예약 취소 로직을 추가할 수도 있습니다.
-    };
+  function gotoback() {
+    navigate("/home");
+  }
 
-    const handleMenuBrowse = (rating) => {
-        console.log(`현재 대기 인원: ${rating}`);
-        // 여기에서 메뉴 둘러보기 로직을 추가할 수도 있습니다.
-    };
-
-    function gotoback() {
-        navigate('/home');
-    }
-
-    return (
-        <div className="threadbackground">
-            <div className="threadcontext">
-                <ThreadList threads={reviews} onCancelReservation={handleCancelReservation} onMenuBrowse={handleMenuBrowse} />
-            </div>
-            <div className="threadbottom">
-                <button className="threadbackbutton"onClick={gotoback}> 홈으로 </button>
-            </div>
-        </div>
-    );
+  return (
+    <div className="threadbackground">
+      <div className="threadcontext">
+        <ThreadList
+          threads={reviews}
+          onCancelReservation={handleCancelReservation}
+          onMenuBrowse={handleMenuBrowse}
+        />
+      </div>
+      <div className="threadbottom">
+        <button className="threadbackbutton" onClick={gotoback}>
+          {" "}
+          홈으로{" "}
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default Thread;
